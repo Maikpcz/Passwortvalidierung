c1ab095e70bdff636b205a340c8930edf2042954
maikpopowicz <maik.popowicz@outlook.com>
2022-11-27 20:16:36 +0100

    Passwort Validirung

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Schreibe dein Passwort");
+        Scanner input = new Scanner(System.in);
+        String passwort = input.nextLine();
+        System.out.println(WoertLaengeUndEineNummerImWort(passwort));
+    }
+
+    public static String WoertLaengeUndEineNummerImWort(String passwort) {
+        int wortlaenge = passwort.length();
+        if (zahlImWort(passwort)) {
+            return "Dein Passwort muss mindestens eine Zahl enthalten";
+        }
+        if (wortlaenge >= 8 ) {
+            return "Dein Passwort ist Gut";}
+        else if (wortlaenge <= 8 ) {
+            return "Das Wort hat zu wenig Buchstaben";
+        }
+        else return "Ungültige Eingabe";
+    }
+    public static boolean zahlImWort(String str) {
+         return str.matches(".*\\d.*");
+
+    }
+}
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void woertLängeWenigerAlsAcht() {
+        String actual = Main.WoertLaengeUndEineNummerImWort("Hallo");
+        assertEquals(actual, "Das Wort hat zu wenig Buchstaben");
+
+    }
+@Test
+    void woertLängeMehrAls8() {
+        String actual = Main.WoertLaengeUndEineNummerImWort("Hallo World");
+        assertEquals(actual, "Dein Passwort ist Gut");
+
+    }
+    @Test
+    void DasPasswortBrauchteineZahl() {
+        boolean actual = Main.zahlImWort("Halloworld");
+        assertEquals(actual, false);
+    }
+
+    @Test
+    void DeinPasswortHatEineZahl() {
+        boolean actual = Main.zahlImWort("HelloWorld1");
+        assertEquals(actual, true);
+    }
+}
